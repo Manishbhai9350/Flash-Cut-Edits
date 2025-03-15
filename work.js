c264c6c249db530bd851b8f45243720e86145c09
Manish <keshraram294@gmail.com>
2025-03-15 13:29:31 +0530

    Some Works Added

--- work.js
+++ work.js
@@ -1,8 +1,87 @@
+import { WorkData } from "./work_data.js";
+
 document.addEventListener("DOMContentLoaded", () => {
+  console.clear();
+  const WorkInfo = JSON.parse(localStorage.getItem("work_data"));
+  if (!WorkInfo) return;
+  const { Type, SubType } = WorkInfo;
+
+  const Data = WorkData[Type];
+  const Categories = Data.Categories;
+  const Category = Categories[SubType];
+  const Works = Category.works
+  const Title = Data.title
+
+  const TitleText = document.querySelector('.works-title')
+  const CategoryText = document.querySelector('.works-sub-type')
+  const WorksContentCon = document.querySelector('.works-content')
+
+  CategoryText.innerHTML = Category.title
+  TitleText.innerHTML = Title
+
+  const IsVideo = Data.type == 'video'
+
+  if(IsVideo){
+    let Clutter = ''
+    Works.forEach(Work => {
+      const Tag = `
+      <div playing="false" class="video-item">
+            <div class="video">
+              <iframe
+                style="width: 100%; height: 100%"
+                src="https://drive.google.com/file/d/${Work.id}/preview"
+                allow="autoplay"
+              ></iframe>
+            </div>
+            <div class="content">
+              <div class="tag">
+                <p>${Work.tag}</p>
+              </div>
+            </div>
+          </div>
+      `
+      Clutter += Tag
+    })
+    WorksContentCon.innerHTML = Clutter
+  } else {
+    let Clutter = ''
+    console.log(Works)
+    Works.forEach(Work => {
+      const Tag = `
+      <div class="thumbnail-item">
+            <div class="thumbnail">
+              <img src="${Work.img}" alt="">
+            </div>
+            <div class="content">
+              <div class="tag">
+                <p>${Work.tag}</p>
+              </div>
+            </div>
+      </div>
+        `
+        Clutter += Tag
+    })
+    WorksContentCon.innerHTML = Clutter
+  }
+
   
-    console.clear()
-    const WorkData = JSON.parse(localStorage.getItem("work_data"));
-    console.log(WorkData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   const Hamburger = document.querySelector(".hamburger");
 
   let IsMobileNavOpen = false;
@@ -71,151 +150,151 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-//   const GraphicDesignCon = document.querySelector(".mobile-nav .item.graphic");
-//   const GraphicContent = GraphicDesignCon.querySelector(".item-content");
-//   const GraphicData = GraphicDesignCon.querySelector(".item-data");
+  //   const GraphicDesignCon = document.querySelector(".mobile-nav .item.graphic");
+  //   const GraphicContent = GraphicDesignCon.querySelector(".item-content");
+  //   const GraphicData = GraphicDesignCon.querySelector(".item-data");
 
-//   const DataHeight = 96;
-//   const ContentHeight = 50;
+  //   const DataHeight = 96;
+  //   const ContentHeight = 50;
 
-//   function CloseGraphics() {
-//     GraphicDesignCon.setAttribute("closed", "true");
-//     gsap.to(GraphicDesignCon.querySelector(".drop-down"), {
-//       rotate: 0,
-//     });
-//     gsap.to(GraphicDesignCon, {
-//       height: ContentHeight,
-//     });
-//   }
+  //   function CloseGraphics() {
+  //     GraphicDesignCon.setAttribute("closed", "true");
+  //     gsap.to(GraphicDesignCon.querySelector(".drop-down"), {
+  //       rotate: 0,
+  //     });
+  //     gsap.to(GraphicDesignCon, {
+  //       height: ContentHeight,
+  //     });
+  //   }
 
-//   GraphicContent.addEventListener("click", (e) => {
-//     const IsClosed = GraphicDesignCon.getAttribute("closed") == "true";
-//     GraphicDesignCon.setAttribute("closed", !IsClosed);
-//     if (IsClosed) {
-//       gsap.to(GraphicDesignCon.querySelector(".drop-down"), {
-//         rotate: 180,
-//       });
-//       gsap.to(GraphicDesignCon, {
-//         height: ContentHeight + DataHeight,
-//       });
-//     } else {
-//       CloseGraphics();
-//     }
-//   });
+  //   GraphicContent.addEventListener("click", (e) => {
+  //     const IsClosed = GraphicDesignCon.getAttribute("closed") == "true";
+  //     GraphicDesignCon.setAttribute("closed", !IsClosed);
+  //     if (IsClosed) {
+  //       gsap.to(GraphicDesignCon.querySelector(".drop-down"), {
+  //         rotate: 180,
+  //       });
+  //       gsap.to(GraphicDesignCon, {
+  //         height: ContentHeight + DataHeight,
+  //       });
+  //     } else {
+  //       CloseGraphics();
+  //     }
+  //   });
 
-//   const ServiceCon = document.querySelector(".item.services");
-//   const ServiceContent = ServiceCon.querySelector(".item-content");
-//   const ServiceItemsCon = ServiceCon.querySelector(".item-data");
-//   const ServiceItems = ServiceItemsCon.querySelectorAll(".item");
+  //   const ServiceCon = document.querySelector(".item.services");
+  //   const ServiceContent = ServiceCon.querySelector(".item-content");
+  //   const ServiceItemsCon = ServiceCon.querySelector(".item-data");
+  //   const ServiceItems = ServiceItemsCon.querySelectorAll(".item");
 
-//   let ServiceHeight = 52;
-//   let ServiceContentHeight = 56;
-//   let ListenerAdded = false;
-//   let ServiceChildHeights = [];
-//   let ContainerHeight = ServiceContentHeight;
-//   let MaxHeight = 0;
-//   function CloseServices() {
-//     ServiceItems.forEach((Item) => {
-//       gsap.to(Item, {
-//         height: ServiceHeight,
-//       });
-//       gsap.to(Item.querySelector(".drop-down"), {
-//         rotate: 0,
-//       });
-//       Item.setAttribute("closed", "true");
-//     });
-//     ContainerHeight = ServiceContentHeight;
-//     ServiceCon.setAttribute("closed", "true");
-//     gsap.to(ServiceContent.querySelector(".drop-down"), {
-//       rotate: 0,
-//     });
-//     gsap.to(ServiceCon, {
-//       height: ContainerHeight,
-//     });
-//   }
+  //   let ServiceHeight = 52;
+  //   let ServiceContentHeight = 56;
+  //   let ListenerAdded = false;
+  //   let ServiceChildHeights = [];
+  //   let ContainerHeight = ServiceContentHeight;
+  //   let MaxHeight = 0;
+  //   function CloseServices() {
+  //     ServiceItems.forEach((Item) => {
+  //       gsap.to(Item, {
+  //         height: ServiceHeight,
+  //       });
+  //       gsap.to(Item.querySelector(".drop-down"), {
+  //         rotate: 0,
+  //       });
+  //       Item.setAttribute("closed", "true");
+  //     });
+  //     ContainerHeight = ServiceContentHeight;
+  //     ServiceCon.setAttribute("closed", "true");
+  //     gsap.to(ServiceContent.querySelector(".drop-down"), {
+  //       rotate: 0,
+  //     });
+  //     gsap.to(ServiceCon, {
+  //       height: ContainerHeight,
+  //     });
+  //   }
 
-//   function AddLister() {
-//     ServiceItems.forEach((Item, idx) => {
-//       const ItemContent = Item.querySelector(".item-content");
-//       const Child = Item.querySelector(".item-data");
-//       ItemContent.addEventListener("click", () => {
-//         let IsClosed = Item.getAttribute("closed") == "true";
-//         Item.setAttribute("closed", !IsClosed);
-//         if (!IsClosed) {
-//           gsap.to(Item, {
-//             height: ServiceChildHeights[idx].closed,
-//           });
-//           gsap.to(Item.querySelector(".drop-down"), {
-//             rotate: 0,
-//           });
-//           CompressService(idx);
-//         } else {
-//           gsap.to(Item, {
-//             height: ServiceChildHeights[idx].total,
-//           });
-//           gsap.to(Item.querySelector(".drop-down"), {
-//             rotate: 180,
-//           });
-//           ExpandService(idx);
-//         }
-//       });
-//     });
-//     ListenerAdded = true;
-//   }
+  //   function AddLister() {
+  //     ServiceItems.forEach((Item, idx) => {
+  //       const ItemContent = Item.querySelector(".item-content");
+  //       const Child = Item.querySelector(".item-data");
+  //       ItemContent.addEventListener("click", () => {
+  //         let IsClosed = Item.getAttribute("closed") == "true";
+  //         Item.setAttribute("closed", !IsClosed);
+  //         if (!IsClosed) {
+  //           gsap.to(Item, {
+  //             height: ServiceChildHeights[idx].closed,
+  //           });
+  //           gsap.to(Item.querySelector(".drop-down"), {
+  //             rotate: 0,
+  //           });
+  //           CompressService(idx);
+  //         } else {
+  //           gsap.to(Item, {
+  //             height: ServiceChildHeights[idx].total,
+  //           });
+  //           gsap.to(Item.querySelector(".drop-down"), {
+  //             rotate: 180,
+  //           });
+  //           ExpandService(idx);
+  //         }
+  //       });
+  //     });
+  //     ListenerAdded = true;
+  //   }
 
-//   function ExpandService(idx) {
-//     gsap.to(ServiceCon, {
-//       height:
-//         ServiceCon.getBoundingClientRect().height +
-//         ServiceChildHeights[idx].items,
-//     });
-//   }
+  //   function ExpandService(idx) {
+  //     gsap.to(ServiceCon, {
+  //       height:
+  //         ServiceCon.getBoundingClientRect().height +
+  //         ServiceChildHeights[idx].items,
+  //     });
+  //   }
 
-//   function CompressService(idx) {
-//     gsap.to(ServiceCon, {
-//       height:
-//         ServiceCon.getBoundingClientRect().height -
-//         ServiceChildHeights[idx].items,
-//     });
-//   }
+  //   function CompressService(idx) {
+  //     gsap.to(ServiceCon, {
+  //       height:
+  //         ServiceCon.getBoundingClientRect().height -
+  //         ServiceChildHeights[idx].items,
+  //     });
+  //   }
 
-//   function OpenService() {
-//     ServiceChildHeights = [];
-//     ServiceItems.forEach((Item) => {
-//       const Child = Item.querySelector(".item-data");
-//       const ItemRect =
-//         Item.querySelector(".item-content").getBoundingClientRect();
-//       const ChildRect = Child.getBoundingClientRect();
-//       const ItemObj = {
-//         closed: ItemRect.height,
-//         total: ItemRect.height + ChildRect.height,
-//         items: ChildRect.height,
-//       };
-//       ServiceChildHeights.push(ItemObj);
-//     });
-//     ContainerHeight += ServiceChildHeights.reduce(
-//       (Prev, Item) => Prev + Item.closed,
-//       0
-//     );
-//     gsap.to(ServiceContent.querySelector(".drop-down"), {
-//       rotate: 180,
-//     });
-//     gsap.to(ServiceCon, {
-//       height: ContainerHeight,
-//     });
-//     if (!ListenerAdded) {
-//       AddLister();
-//     } else {
-//     }
-//   }
+  //   function OpenService() {
+  //     ServiceChildHeights = [];
+  //     ServiceItems.forEach((Item) => {
+  //       const Child = Item.querySelector(".item-data");
+  //       const ItemRect =
+  //         Item.querySelector(".item-content").getBoundingClientRect();
+  //       const ChildRect = Child.getBoundingClientRect();
+  //       const ItemObj = {
+  //         closed: ItemRect.height,
+  //         total: ItemRect.height + ChildRect.height,
+  //         items: ChildRect.height,
+  //       };
+  //       ServiceChildHeights.push(ItemObj);
+  //     });
+  //     ContainerHeight += ServiceChildHeights.reduce(
+  //       (Prev, Item) => Prev + Item.closed,
+  //       0
+  //     );
+  //     gsap.to(ServiceContent.querySelector(".drop-down"), {
+  //       rotate: 180,
+  //     });
+  //     gsap.to(ServiceCon, {
+  //       height: ContainerHeight,
+  //     });
+  //     if (!ListenerAdded) {
+  //       AddLister();
+  //     } else {
+  //     }
+  //   }
 
-//   ServiceContent.addEventListener("click", (e) => {
-//     const IsClosed = ServiceCon.getAttribute("closed") == "true";
-//     ServiceCon.setAttribute("closed", !IsClosed);
-//     if (IsClosed) {
-//       OpenService();
-//     } else {
-//       CloseServices();
-//     }
-//   });
+  //   ServiceContent.addEventListener("click", (e) => {
+  //     const IsClosed = ServiceCon.getAttribute("closed") == "true";
+  //     ServiceCon.setAttribute("closed", !IsClosed);
+  //     if (IsClosed) {
+  //       OpenService();
+  //     } else {
+  //       CloseServices();
+  //     }
+  //   });
 });
--- work_data.js
+++ work_data.js
@@ -1,3 +1,220 @@
 export const WorkData = {
-    
+    basic_editing:{
+        title:'Basic Video Editing',
+        type:'video',
+        Categories:
+            {
+                podcast_editing:{
+                    title:'Podcasts Editing',
+                },
+                business_ads:{
+                    title:'Business ADS'
+                },
+                gym_edits:{
+                    title:"GYM Edits"
+                },
+                animated_edits:{
+                    title:"Animated Edits"
+                },
+                faceless_edits:{
+                    title:"Faceless Edits"
+                },
+                health_edits:{
+                    title:"Health Edits"
+                },
+                tech_reel:{
+                    title:"Tech Reels"
+                },
+                gaming_reel:{
+                    title:"Gaming Reels"
+                },
+                interview_reel:{
+                    title:"Interview Reels"
+                },
+                education_reel:{
+                    title:"Education Reels"
+                },
+            }
+    },
+    advance_editing:{
+        title:'Advance Video Editing',
+        type:'video',
+        Categories:
+            {
+                podcast_editing:{
+                    title:'Podcasts Editing',
+                    works:[
+                        {
+                            id:'1SQHqVOd3aKNn6hDmYeX1Z7RNbENdI5qJ',
+                            tag:'Podcast Edit'
+                        },
+                        {
+                            id:'1Tk3IeEDi1nXiWg4v3QxyrL0d4c25nWpA',
+                            tag:'Podcast Edit'
+                        },
+                        {
+                            id:'13XrmwqUya4NmWaouXFGLZt6a30gFmgpS',
+                            tag:'Podcast Edit'
+                        },
+                    ]
+                },
+                business_ads:{
+                    title:'Business ADS'
+                },
+                gym_edits:{
+                    title:"GYM Edits"
+                },
+                animated_edits:{
+                    title:"Animated Edits"
+                },
+                faceless_edits:{
+                    title:"Faceless Edits",
+                    works:[
+                        {
+                            id:'13ysTh4cBhDUL1ZMyT_58W8o4D2XjaQUG',
+                            tag:'Faceless Video Editing'
+                        },
+                        {
+                            id:'1y22N8CcM6rRYle7YZZPtFnK_6Blgf4r1',
+                            tag:'Faceless Video Editing'
+                        },
+                        {
+                            id:'10XmisHlQM4otA3JmLh4AonY_4xM6lMbw',
+                            tag:'Faceless Video Editing'
+                        },
+                    ]
+                },
+                health_edits:{
+                    title:"Health Edits"
+                },
+                tech_reel:{
+                    title:"Tech Reels",
+                    works:[
+                        {
+                            id:'1UWNyCn8ISOHpaQ93yY2OqXieGdcerJNl',
+                            tag:'Tech Reel'
+                        },
+                        {
+                            id:'1B2Tj-5c7h-4cP_koU-tCEue5O9qM2doB',
+                            tag:'Tech Reel'
+                        },
+                        {
+                            id:'1opLwEebKUDc-9wkxvMS51ZxDfcdvlnIW',
+                            tag:'Tech Reel'
+                        },
+                        {
+                            id:'1VqZOIcJQ4ouyj8TBM_CBwGIj5KnrmBU6',
+                            tag:'Tech Reel'
+                        },
+                        {
+                            id:'1Gz--jZ0Th2BBp89giGpCaUYP2-6mAnoh',
+                            tag:'Tech Reel'
+                        },
+                    ]
+                },
+                gaming_reel:{
+                    title:"Gaming Reels"
+                },
+                interview_reel:{
+                    title:"Interview Reels",
+                    works:[
+                        {
+                            id:'1SQHqVOd3aKNn6hDmYeX1Z7RNbENdI5qJ',
+                            tag:'Interview Reel'
+                        },
+                        {
+                            id:'1Tk3IeEDi1nXiWg4v3QxyrL0d4c25nWpA',
+                            tag:'Interview Reel'
+                        },
+                        {
+                            id:'13XrmwqUya4NmWaouXFGLZt6a30gFmgpS',
+                            tag:'Interview Reel'
+                        },
+                    ]
+                },
+                education_reel:{
+                    title:"Education Reels"
+                },
+            }
+    },
+    graphic_designing:{
+        title:'Graphic Designing',
+        type:'thumbnail',
+        Categories:{
+            documentary:{
+                title:'Documentary Thumbnail'
+            },
+            education:{
+                title:'Education Thumbnails'
+            },
+            fitness:{
+                title:'Fitness Thumbnails'
+            },
+            gaming:{
+                title:'Gaming Thumbnails'
+            },
+            podcast:{
+                title:'Podcast Thumbnails'
+            },
+            tech:{
+                title:'Tech Thumbnails'
+            },
+            trading:{
+                title:'Trading Thumbnails'
+            },
+            vlog_lifestyle:{
+                title:'Vlog Lifestyle Thumbnails',
+                works:[
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail1.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail2.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail3.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail4.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail5.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail6.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail7.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail8.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail9.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail10.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail11.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                    {
+                        img:'./public/thumbnails/vlog_lifestyle/thumbnail12.jpg',
+                        tag:"Vlog Lifestyle Thumbnail"
+                    },
+                ]
+            },
+        }
+    }
 }
